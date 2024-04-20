a43c0c87313b5220f83a4a6450963a271f77dd2b
Benjamin Cates <thebenjicat@gmail.com>
2024-04-19 23:31:49 -0700

    Add popup window when analyze button hit

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,5 @@
+import { get_captions } from "./captions";
+
 function injectButtons() {
   const videoContainers = document.querySelectorAll("#details");
 
@@ -12,9 +14,24 @@ function injectButtons() {
         button.style.marginLeft = "5px";
         titleElement.parentNode!.insertBefore(button, titleElement.nextSibling);
 
-        button.addEventListener("click", function (e) {
-          alert("Clickbait detection for: " + titleElement.textContent!.trim());
+        button.addEventListener("click", function (e: MouseEvent) {
+          e.preventDefault();
           e.stopPropagation();
+          const video_id = ((e.target as HTMLElement).parentNode as HTMLAnchorElement).href.replace("https://www.youtube.com/watch?v=","");
+          const popup = document.createElement("div");
+          popup.classList.add("yt_analyzer_popup");
+          popup.style.left = e.pageX+"px";
+          popup.style.top = e.pageY+"px";
+          popup.style.background = "#222222";
+          popup.style.color = "#FFFFFF";
+          popup.style.fontSize = "1.8rem";
+          popup.style.zIndex = "4000";
+          popup.textContent = "Getting captions for "+video_id;
+          document.body.appendChild(popup);
+          get_captions(video_id).then(captions => {
+            popup.textContent = "Analyzing captions for " + video_id;
+            popup.textContent += ". Starts with: \"" + captions[0].text + "\"";
+          })
         });
       }
     }
