013c5a9cbb27a29894d1c7259749e943eb42c902
Priyansh Shah <Priyanshpokemon@gmail.com>
2024-04-20 02:10:05 -0700

    cant get fonts working but i will work on lock in mode now!!!

--- public/script.js
+++ public/script.js
@@ -1,12 +1,15 @@
-
-
 function loadSettings() {
   chrome.storage.local.get(
     ["hideRecommendations", "hideComments"],
     function (result) {
       document.getElementById("hide-recommendations").checked =
-        !!result.hideRecommendations;
-      document.getElementById("hide-comments").checked = !!result.hideComments;
+        result.hideRecommendations;
+      document.getElementById("hide-comments").checked = result.hideComments;
+      document.getElementById("hide-home-feed").checked = result.hideComments;
+      document.getElementById("hide-shorts").checked = result.hideComments;
+      document.getElementById("hide-notifications").checked =
+        result.hideComments;
+      document.getElementById("stop-autoplay").checked = result.hideComments;
     }
   );
 }
@@ -17,10 +20,19 @@ function saveSettings() {
     "hide-recommendations"
   ).checked;
   const hideComments = document.getElementById("hide-comments").checked;
+  const hideHomeFeed = document.getElementById("hide-home-feed").checked;
+  const hideShorts = document.getElementById("hide-shorts").checked;
+  const hideNotifications =
+    document.getElementById("hide-notifications").checked;
+  const stopAutoplay = document.getElementById("stop-autoplay").checked;
 
   chrome.storage.local.set({
     hideRecommendations: hideRecommendations,
     hideComments: hideComments,
+    hideHomeFeed: hideHomeFeed,
+    hideShorts: hideShorts,
+    hideNotifications: hideNotifications,
+    stopAutoplay: stopAutoplay,
   });
 }
 
@@ -31,6 +43,15 @@ document
 document
   .getElementById("hide-comments")
   .addEventListener("change", saveSettings);
-
+document
+  .getElementById("hide-home-feed")
+  .addEventListener("change", saveSettings);
+document.getElementById("hide-shorts").addEventListener("change", saveSettings);
+document
+  .getElementById("hide-notifications")
+  .addEventListener("change", saveSettings);
+document
+  .getElementById("stop-autoplay")
+  .addEventListener("change", saveSettings);
 // Load settings when the script is loaded
 loadSettings();
--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,32 +1,66 @@
 // Function to hide elements based on settings
 function applyUserSettings() {
   chrome.storage.local.get(
-    ["hideRecommendations", "hideComments"],
+    ["hideRecommendations", "hideComments", "hideHomeFeed", "hideShorts", "hideNotifications", "stopAutoplay"],
     function (result) {
       // Hide or show recommendations based on the stored setting
       const recommendations = document.querySelectorAll(
         "#items.ytd-watch-next-secondary-results-renderer"
       );
-      recommendations.forEach((el) => {
-        (el as HTMLElement).style.display = result.hideRecommendations ? "none" : "block";
+      Array.from(recommendations).forEach((el) => {
+        (el as HTMLElement).style.display = result.hideRecommendations
+          ? "none"
+          : "block";
       });
-
-      // Hide or show comments based on the stored setting
-      const comments = document.querySelector("#comments");
-      if (comments) {
-        (comments as HTMLElement).style.display = result.hideComments ? "none" : "block";
-      }
+      const comments = document.querySelectorAll("#comments");
+      Array.from(comments).forEach((el) => {
+        (el as HTMLElement).style.display = result.hideComments
+          ? "none"
+          : "block";
+      });
+      const home_feed = document.querySelectorAll("#primary");
+      Array.from(home_feed).forEach((el) => {
+        (el as HTMLElement).style.display = result.hideHomeFeed
+          ? "none"
+          : "block";
+      });
+      const shorts = document.querySelectorAll("[is-shorts]");
+      Array.from(shorts).forEach((el) => {
+        (el as HTMLElement).style.display = result.hideShorts
+          ? "none"
+          : "block";
+      });
+      const notifications = document.querySelectorAll(
+        "#notification-preference"
+      );
+      Array.from(notifications).forEach((el) => {
+        (el as HTMLElement).style.display = result.hideNotifications
+          ? "none"
+          : "block";
+      });
+      const autoplay = document.querySelectorAll("#autoplay-checkbox");
+      Array.from(autoplay).forEach((el) => {
+        (el as HTMLElement).style.display = result.stopAutoplay
+          ? "none"
+          : "block";
+      });
     }
   );
 }
 
+// </div>
 window.addEventListener("load", applyUserSettings);
 
 // Listen for changes in the storage and apply the user settings if there's a change
 chrome.storage.onChanged.addListener(function (changes, namespace) {
   for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
     if (
-      (key === "hideRecommendations" || key === "hideComments") &&
+      (key === "hideRecommendations" ||
+        key === "hideComments" ||
+        key === "hideHomeFeed" ||
+        key === "hideShorts" ||
+        key === "hideNotifications" ||
+        key === "stopAutoplay") &&
       oldValue !== newValue
     ) {
       applyUserSettings();
@@ -34,5 +68,4 @@ chrome.storage.onChanged.addListener(function (changes, namespace) {
   }
 });
 
-// Initialize the settings on load
 applyUserSettings();
