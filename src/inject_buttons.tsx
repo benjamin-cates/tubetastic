0aa9b7a9d393a1287ce0ea89feef108927a95081
Benjamin Cates <thebenjicat@gmail.com>
2024-04-21 04:22:17 -0700

    Fix typo oops

--- src/inject_buttons.tsx
+++ src/inject_buttons.tsx
@@ -45,7 +45,7 @@ function injectButtons() {
           popup.style.color = "#FFFFFF";
           popup.style.fontSize = "1.8rem";
           popup.style.zIndex = "4000";
-          popup_message.textContent = title+"\nReading video metadata";
+          popup_message.innerText = title+"\nReading video metadata";
           const close_button = document.createElement("button");
           close_button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#FFFFFF" d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/></svg>`;
           close_button.classList.add("popup_close");
@@ -65,10 +65,10 @@ function injectButtons() {
           worker_comms(video_id).then(async data_response => {
             try {
               if(data_response.captions.length > 0) {
-                popup_message.textContent = title+"\nTranscript found. \nAnaylzing...";
+                popup_message.innerText = title+"\nTranscript found. \nAnalyzing...";
               }
               else {
-                popup_message.textContent = title+"\nTranscript not found. \nAnalyzing description...";
+                popup_message.innerText = title+"\nTranscript not found. \nAnalyzing description...";
               }
               const video_data: VideoData = {
                 thumbnail: await getThumbnailUrl(video_id,0),
