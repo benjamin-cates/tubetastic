2828d826388774c618e6c4a10b34cd93d012f040
Jack Nguyen <nguyenminhtam7124@gmail.com>
2024-04-20 19:26:41 -0700

    Added like count

--- src/description_api.ts
+++ src/description_api.ts
@@ -3,7 +3,6 @@ const YOUTUBE_API_KEY = "AIzaSyD3x-GATNpXBT4oPsSJr2XbLyEHS7lEZnc";
 
 const get_description_api = async (video_id: string): Promise<string[]> => {
     const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${video_id}&key=${YOUTUBE_API_KEY}&fields=items(snippet(description))`);  
-    console.log("Response is ", response);
     const data = await response.json();
     if(data.error) return [];
     return data.items.map((item: any) => {
@@ -11,5 +10,13 @@ const get_description_api = async (video_id: string): Promise<string[]> => {
     });
   
 }
-
-export { get_description_api };
+const get_statistics_api = async (video_id: string): Promise<any[]> => {
+    const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?part=statistics&id=${video_id}&key=${YOUTUBE_API_KEY}`);
+    const data = await response.json();
+    if(data.error) return [];
+    return data.items.map((item: any) => {
+        return item.statistics;
+    });
+  
+}
+export { get_description_api, get_statistics_api };
