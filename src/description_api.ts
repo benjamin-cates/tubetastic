62435f80deed8457c31652c089175f76950a39f8
Jack Nguyen <nguyenminhtam7124@gmail.com>
2024-04-20 17:49:59 -0700

    Get description with Youtube API

--- src/description_api.ts
+++ src/description_api.ts
@@ -1,11 +1,10 @@
 
 const YOUTUBE_API_KEY = "AIzaSyD3x-GATNpXBT4oPsSJr2XbLyEHS7lEZnc";
 
-const get_description_api = async (video_id: string) => {
-    const videoId = "YOUR_VIDEO_ID";
-    const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${YOUTUBE_API_KEY}&fields=items(snippet(description))`);  
+const get_description_api = async (video_id: string): Promise<string[]> => {
+    const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${video_id}&key=${YOUTUBE_API_KEY}&fields=items(snippet(description))`);  
+    console.log("Response is ", response);
     const data = await response.json();
-    console.log('description api', response);
     if(data.error) return [];
     return data.items.map((item: any) => {
         return item.snippet.description;
