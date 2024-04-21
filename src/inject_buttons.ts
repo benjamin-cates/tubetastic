d370de7900e64eddf90d56e2a89192ef4312e0ca
Benjamin Cates <thebenjicat@gmail.com>
2024-04-20 17:35:58 -0700

    Change loading prompt

--- src/inject_buttons.ts
+++ src/inject_buttons.ts
@@ -51,7 +51,7 @@ function injectButtons() {
           popup.style.color = "#FFFFFF";
           popup.style.fontSize = "1.8rem";
           popup.style.zIndex = "4000";
-          popup_message.textContent = "Getting captions for "+video_id;
+          popup_message.textContent = title+"\nReading video metadata";
           const close_button = document.createElement("button");
           close_button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#FFFFFF" d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/></svg>`;
           close_button.classList.add("popup_close");
@@ -71,10 +71,10 @@ function injectButtons() {
           worker_comms(video_id).then(async data_response => {
             try {
               if(data_response.captions.length > 0) {
-                popup_message.textContent = "Captions for " + video_id + ". Anaylzing...";
+                popup_message.textContent = title+"\nTranscript found. \nAnaylzing...";
               }
               else {
-                popup_message.textContent = "Captions not found for " + video_id + ". Analyzing description...";
+                popup_message.textContent = title+"\nTranscript not found. \nAnalyzing description...";
               }
               const video_data: VideoData = {
                 thumbnail: await getThumbnailUrl(video_id,0),
@@ -131,7 +131,7 @@ function injectButtons() {
                 el.appendChild(text);
                 out.appendChild(el);
               }
-              popup_message.textContent = "";
+              popup_message.textContent = title;
               popup.appendChild(out);
             }
             catch(e) {
